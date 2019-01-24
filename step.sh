ccc1b5f249ce49d4dff77f85fad1b66c73ca81ee
xavi <javier.anyo@gigigo.com>
2019-01-24 12:33:00 +0100

    New: Added bundle conversion

--- step.sh
+++ step.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+#=======================================
+# Main
+#=======================================
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+#step_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+temp_path=$PWD
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+echo "building universal apk path"
+echo "Getting app bundle from ${aab_path}"
+echo "Signing with ${keystore_url} key and alias ${keystore_alias}"
+
+bundletool="${temp_path}/bundletool.jar"
+keystore="${temp_path}/keystore.jks"
+source="https://github.com/google/bundletool/releases/download/0.8.0/bundletool-all-0.8.0.jar"
+
+
+# Building
+aab_output_path="${temp_path}/output/bundle"
+aab_output="${aab_output_path}/app.apks"
+apk_output_path="${temp_path}/output/apk"
+apk_output="${apk_output_path}/universal.apk"
+
+mkdir -p "${aab_output_path}" &
+mkdir -p "${apk_output_path}" &
+wait
+curl -o "keystore.jks" "${keystore_url}" &
+wget "${source}" --output-document="${bundletool}" &
+wait
+exec java -jar "${bundletool}" build-apks --bundle="${aab_path}" --output="${aab_output}" --mode=universal --ks=${keystore} --ks-pass=pass:"${keystore_password}" --ks-key-alias="${keystore_alias}" --key-pass=pass:"${keystore_alias_password}" &
+wait
+echo "APK created in ${apk_output_path}"
+exec unzip ${aab_output} -d ${apk_output_path} &
+
+envman add --key BITRISE_APK_PATH --value ${apk_output}
+
+echo
+exit 0
